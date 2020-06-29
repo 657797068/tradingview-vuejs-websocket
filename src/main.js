84669a0166e4bf933a3414666578cc49a3648f00
zhouxinyuan <xinyuan_meil@163.com>
2020-06-29 20:46:01 +0800

    上传项目

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './main.css'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+Vue.use(Antd)
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
